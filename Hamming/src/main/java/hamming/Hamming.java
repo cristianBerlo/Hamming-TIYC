6e956e8b2396356f71a33d8ce9e3c0bb448b0066
Cristian <cristianeberlo@gmail.com>
2026-04-17 02:24:09 -0300

    Agregada funcion para calcular sindrome

--- Hamming/src/main/java/hamming/Hamming.java
+++ Hamming/src/main/java/hamming/Hamming.java
@@ -1,24 +1,50 @@
 package hamming;
-
-import java.math.BigInteger;
 import java.util.BitSet;
 
 public class Hamming {
 	private int control; // cantidad de bits de control
 	private int lenght; //largo del mensaje resultante (informacion + controles) 
-public Hamming(int lenght, int control) {
-	this.control=control;
+	
+public Hamming(int lenght) {
 	this.lenght=lenght;
+    control = 0;
+    while (Math.pow(2, control) < (lenght + 1)) {
+        control++;
+    }
 }
+
+public BitSet hammingTrans(BitSet hamming) {
+	BitSet string;
+	BitSet sindrome= new BitSet(control);
+	for(int c=0;c<control;c++) { //calculo bits de control
+		  boolean xor = false; 
+		     int mask = 1 << c;
+			 for (int h = mask; h < lenght; h++) {
+				 if ((h & mask) != 0) {
+		                xor ^= hamming.get(h-1);
+		                //System.out.print(hamming.get(h-1) +" "+c+" "+" "+h+"  ");
+		         
+				 }
+			 }
+			 sindrome.set(c, xor);
+		    }
+	return sindrome;
+}
+public BitSet parity(BitSet string) {
+	int xor= string.cardinality()%2;
+	string.set(lenght, xor);
+	return string;
+}
+
 public BitSet hamming(BitSet string) {
 	BitSet hamming = new BitSet(lenght);
 	int j = 0;
 	for(int i=0;i< lenght;i++) {
-	  if( (i & (i+1)) !=0 ) { // si la posicion es la de un bit de control lo calculo
+	  if( (i & (i+1)) !=0 ) { // seteo bits no de control
 		  hamming.set(i, string.get(j));
 		  j++;
 	  }}
-	  for(int c=0;c<control;c++) {
+	  for(int c=0;c<control;c++) { //calculo bits de control
 	  boolean xor = false; 
 	     int mask = 1 << c;
 		 for (int h = mask; h < lenght; h++) {
@@ -28,7 +54,8 @@ public BitSet hamming(BitSet string) {
 		 }
 		 hamming.set(mask-1, xor);
 	    }
-	
+		int xor= string.cardinality()%2; //cardinality devuelve la cantidad de bits seteados a 1, el resto puede ser 0 si es par o 1 si es impar
+		string.set(lenght, xor); // agrego el bit de pariedad en la ultima posicion
 	return hamming; 
 }
 
